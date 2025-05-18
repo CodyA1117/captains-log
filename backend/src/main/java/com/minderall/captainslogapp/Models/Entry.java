491aa20e58118c72ad8b946c34a882978ccfe33e
CodyA1117 <adamscody079@gmail.com>
2025-05-18 15:24:34 -0500

    added the entry class, this will control all captains log posts.

--- backend/src/main/java/com/minderall/captainslogapp/Models/Entry.java
+++ backend/src/main/java/com/minderall/captainslogapp/Models/Entry.java
@@ -1,4 +1,104 @@
 package com.minderall.captainslogapp.Models;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Entry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String note;
+
+    private Integer energy;
+    private Integer mood;
+
+    private String promptUsed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "entry", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EntryTagData> tagData;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Integer getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(Integer energy) {
+        this.energy = energy;
+    }
+
+    public Integer getMood() {
+        return mood;
+    }
+
+    public void setMood(Integer mood) {
+        this.mood = mood;
+    }
+
+    public String getPromptUsed() {
+        return promptUsed;
+    }
+
+    public void setPromptUsed(String promptUsed) {
+        this.promptUsed = promptUsed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<EntryTagData> getTagData() {
+        return tagData;
+    }
+
+    public void setTagData(List<EntryTagData> tagData) {
+        this.tagData = tagData;
+    }
 }
