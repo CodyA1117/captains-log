8eb80d9cce5591a0c91d4fa599bd48dd7c21b236
CodyA1117 <adamscody079@gmail.com>
2025-05-18 08:24:16 -0500

    inital build of captains log app with front end and back end folders created, React Front, and Java/Spring for Back.

---
+++ backend/src/main/java/com/minderall/captainslogapp/CaptainslogappApplication.java
@@ -0,0 +1,13 @@
+package com.minderall.captainslogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptainslogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptainslogappApplication.class, args);
+	}
+
+}
